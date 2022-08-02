90afce86f298a3b30bab8fb475b29f6002de9449
Vasiliy Volgin <82467534+VasiliiV@users.noreply.github.com>
2022-08-02 13:13:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const hostname = '0.0.0.0'
 const port = 3000
 const notes = [];
 
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
